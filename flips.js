1c1ad89429b70f320430676bec0831d1d734dcf0
Savannah Ingle <sningle@email.unc.edu>
2022-02-14 17:43:18 -0500

    pure agony m8. changing the print statements.

--- flips.js
+++ flips.js
@@ -3,12 +3,21 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
 args["number"]
-const flips = args.number | 1
+const flips = args.number | -1
 var i = 0
 const flip_array = [];
 while (i < flips) {
     flip_array[i] = coinFlip();
     i++;
+}
+if (flips == -1) {
+    flip_array[i] = coinFlip();
+    if (flip_array[i] == "heads") {
+        console.log("{ heads: 1 }")
+    } else {
+        console.log("{ tails: 1 }")
+    }
+    process.exit(1);
 }
 
 const map = countFlips(flip_array)
