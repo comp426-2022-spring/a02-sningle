9d3ee15d6e7aa816d53bb59f8b01a230f0f23354
Savannah Ingle <69727315+sningle@users.noreply.github.com>
2022-04-23 21:16:38 -0400

    Update flip.js

--- flip.js
+++ flip.js
@@ -2,4 +2,3 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
 // Call the coinFlip function and put the return into STDOUT
 console.log(coinFlip())
-// console.log(flipACoin("heads"));
